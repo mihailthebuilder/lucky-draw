08a7d407e1f0337b3df6c901385587d13b0a39de
Mihail Marian <mihailmarian12@gmail.com>
2022-11-16 07:34:41 +0000

    fix tests

--- hardhat/test/LuckyDraw.ts
+++ hardhat/test/LuckyDraw.ts
@@ -1,12 +1,7 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("LuckyDraw", function () {
-
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployContractFixtureWith0Balance() {
     const factory = await ethers.getContractFactory("LuckyDraw");
     const contract = await factory.deploy(0);
@@ -21,30 +16,40 @@ describe("LuckyDraw", function () {
 
   describe("Deployment", function () {
     it("Should have an address", async function () {
-      const { contract } = await loadFixture(deployContractFixtureWith0Balance);
+      const { contract } = await deployContractFixtureWith0Balance();
       expect(contract.address).to.not.be.equal("");
     })
 
     it("Should set the balance of 0", async function () {
-      const { contract } = await loadFixture(deployContractFixtureWith0Balance);
+      const { contract } = await deployContractFixtureWith0Balance();
       expect(await contract.balance()).to.equal(0);
     })
 
     it("Should set the balance of 10", async function () {
-      const { contract } = await loadFixture(deployContractFixtureWith10Balance);
+      const { contract } = await deployContractFixtureWith10Balance();
       expect(await contract.balance()).to.equal(10);
     })
   })
 
   describe("Functionality", function () {
     it("Draw adds 1 or stays at 0 if balance is 0", async function () {
-      const { contract } = await loadFixture(deployContractFixtureWith0Balance);
+      const { contract } = await deployContractFixtureWith0Balance();
 
       const txn = await contract.draw();
       await txn.wait();
 
       const balance = await contract.balance();
       expect([1, 0]).to.contain(balance.toNumber());
+    })
+
+    it("Draw adds 1 or reduces by 1 if balance is 10", async function () {
+      const { contract } = await deployContractFixtureWith10Balance();
+
+      const txn = await contract.draw();
+      await txn.wait();
+
+      const balance = await contract.balance();
+      expect([9, 11]).to.contain(balance.toNumber());
     })
   })
 })
