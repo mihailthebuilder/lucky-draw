04b583ef531fbad434f932faf130106b058cadf0
Mihail Marian <mihailmarian12@gmail.com>
2022-11-16 19:14:27 +0000

    deploy contract locally

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,23 +1,27 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const factory = await ethers.getContractFactory("LuckyDraw");
+  const contract = await factory.deploy(1000);
+  await contract.deployed();
 
-  await lock.deployed();
+  console.log("LuckyDraw address: ", contract.address);
+};
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+runMain();
