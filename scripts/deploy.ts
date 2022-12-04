5da18ead208a7281dc736219d4e35eab3f84e108
Mihail Marian <mihailmarian12@gmail.com>
2022-12-04 12:38:03 +0000

    deployment of new contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,7 +8,7 @@ const main = async () => {
   console.log("Account balance: ", accountBalance.toString());
 
   const factory = await ethers.getContractFactory("LuckyDraw");
-  const contract = await factory.deploy(1000);
+  const contract = await factory.deploy(ethers.utils.parseEther("0.000001"), { value: ethers.utils.parseEther("0.0001") });
   await contract.deployed();
 
   console.log("LuckyDraw address: ", contract.address);
