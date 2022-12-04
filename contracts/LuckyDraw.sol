89174e68000bfa3189e02400582c0e9da4952641
Mihail Marian <mihailmarian12@gmail.com>
2022-12-04 10:22:41 +0000

    remove console.sol

--- contracts/LuckyDraw.sol
+++ contracts/LuckyDraw.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-// Uncomment this line to use console.log
-import "hardhat/console.sol";
-
 contract LuckyDraw {
     uint public prize;
 
